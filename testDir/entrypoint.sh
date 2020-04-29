df30ac4c51dafc285aecdc862bdfbc0c39145fac
Michael Beukman <michael.beukman@gmail.com>
2020-04-29 07:09:43 +0200

    stuff

--- testDir/entrypoint.sh
+++ testDir/entrypoint.sh
@@ -1,5 +1,2 @@
-#!/bin/sh -l
-
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+#!/bin/sh
+sh -c "docker run -e MYSQL_ROOT_PASSWORD=root -d -p 3306:3306 mysql:5.7 --port=3306 --lower_case_table_names=1"
